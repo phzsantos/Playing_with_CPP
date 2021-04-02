2aa06230c9be162cf8cc43c9bbb891e2d5e28c8c
phzsantos <phzsantos2002@gmail.com>
2021-04-02 10:04:59 -0300

    Logica pronta, entrada e help prontos; funcionando

---
+++ 02-Dobraduras/project.cpp
@@ -0,0 +1,25 @@
+#include "project.h"
+
+void _help(){
+	std::cout << "--->>>::::: CALCULADORA DE DOBRADURAS :::::<<<---\n"
+		     "\nPara iniciar o programa:\n"
+		     "./a.out --start [quantas vezes foi dobrado]\n";
+}
+
+int calculo( int numero ){
+	return numero * 2;
+}
+
+void _start( int argc, char ** argv ){
+	if(argc > 2){
+		std::string parametro = argv[1];
+		int numero = std::stoi( argv[2] );
+		if( parametro == "--start" || parametro == "-s" ){
+			std::cout << "Esse é o numero de pedaços de papel que você vai ter " << calculo( numero ) << '\n';
+		}else{
+			_help();
+		} 
+	}else{
+	_help();
+	}
+}
---
+++ 02-Dobraduras/project.h
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <cstdlib>
+
+void _help();
+int _calculo( int );
+void _start( int, char ** );
