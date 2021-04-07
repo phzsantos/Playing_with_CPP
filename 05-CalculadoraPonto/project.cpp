897cc25730cb458eccf29d1fd33744a4f476d989
phzsantos <phzsantos2002@gmail.com>
2021-04-06 21:16:45 -0300

    Codigo pronto; Compilando; Funcionando

---
+++ 05-CalculadoraPonto/project.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int hr_1 = {0}, hr_2 = {0}, min_1 = {0}, min_2 = {0}, hr_t ={0}, min_t = {0};
+	
+	cout << "Que horas iniciais: ";
+	cin >> hr_1;
+       	cout << '\n';
+
+	cout << "Minutos iniciais: ";
+	cin >> min_1;
+	cout << '\n';
+
+	cout << "Horas para somar: ";
+	cin >> hr_2;
+	cout << '\n';
+
+	cout << "Minutos para somar: ";
+	cin >> min_2;
+	cout << '\n';
+
+	hr_t = hr_1 + hr_2;
+	min_t = min_1 + min_2;
+
+	while(min_t >= 60){
+		hr_t++;
+		min_t = min_t - 60;
+	}	
+
+	cout << "Essa é hora que você deve voltar do almoço: " << hr_t << ":" << min_t << '\n';
+	return 0;
+}
